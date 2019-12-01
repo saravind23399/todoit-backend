02691d8b4e3f7e77a4f0f466eddf530fbd411714
Aravind S <saravind23399@gmail.com>
2019-12-01 23:05:29 +0530

    Added Relationships

--- models/ToDo.py
+++ models/ToDo.py
@@ -1,10 +1,12 @@
 from server import db
+from sqlalchemy import ForeignKey
 
 # Model - ToDo Item
 class ToDo(db.Model):
     id = db.Column(db.Integer, primary_key = True, )
     title = db.Column(db.String(100))
     description = db.Column(db.String(100))
+    created_by = db.Column(db.Integer, ForeignKey('users.id'))
 
     def __init__(self, id, title, description):
         self.id = id
--- models/User.py
+++ models/User.py
@@ -1,10 +1,12 @@
 from server import db
+from sqlalchemy.orm import relationship
 
 # Model - User Item
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True, )
     username = db.Column(db.String(100))
     about = db.Column(db.String(100))
+    todos = relationship("todos")
 
     def __init__(self, id, username, about):
         self.id = id
